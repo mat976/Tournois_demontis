64bc265e69903a63255379d74511187760c45820
Mat911 <matisse.demontis@gmail.com>
2025-05-20 08:45:36 +0400

    Initial commit

---
+++ src/main/java/com/example/tournois_demontis/TournoisDemontisApplication.java
@@ -0,0 +1,13 @@
+package com.example.tournois_demontis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournoisDemontisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournoisDemontisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tournois_demontis/TournoisDemontisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tournois_demontis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournoisDemontisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
